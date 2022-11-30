fd350e781a6bc9c027489b7f442f5cfd3aad1cce
AzmanovaEvelina <azmanova.evelina.r@gmail.com>
2022-11-30 23:04:57 +0500

    TestView and TestController

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,11 +13,16 @@ namespace WebApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+               name: "Test",
+               url: "test/{action}",
+               defaults: new { controller = "Test", action = "PrintMessage" }
+           );
         }
     }
 }

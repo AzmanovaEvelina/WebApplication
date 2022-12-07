240f294fadfcf1e9fa018f298d4ff1fdf218305e
shabalin-AA <92672124+shabalin-AA@users.noreply.github.com>
2022-12-07 12:18:02 +0500

    Add files via upload

---
+++ Controllers/TimetableController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReadExcel;
+
+namespace WebApplication.Controllers
+{
+    public class TimetableController : Controller
+    {
+        // GET: Timetable
+        public ActionResult Timetable()
+        {
+            string excelName = "test.xlsx";
+            string pathToExcel = @"D:\Downloads";
+            ViewBag.Week = $@"{pathToExcel}\{excelName}";
+            ViewBag.Week = ExcelToTimetable.ConvertExcelToTimetable($@"{pathToExcel}\{excelName}");
+            return View();
+        }
+    }
+}

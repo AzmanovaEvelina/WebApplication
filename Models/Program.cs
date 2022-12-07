ec7f46cbcbf928213b412f69806a96725c377ed2
shabalin-AA <92672124+shabalin-AA@users.noreply.github.com>
2022-12-07 13:33:44 +0500

    Update Program.cs

--- Models/Program.cs
+++ Models/Program.cs
@@ -5,16 +5,49 @@ namespace ReadExcel
 {
     class ExcelToTimetable
     {
+        static int toMonth(string month)
+        {
+            if (month == "марта") return 3;
+            if (month == "апреля") return 4;
+            if (month == "октября") return 10;
+            if (month == "ноября") return 11;
+            return 0;
+        }
+
+        static int daysInMonths(int month)
+        {
+            if (month < 0) return 0;
+            if (month == 2) return 28;
+            return 30 + month % 2 + daysInMonths(month - 1);
+        }
+
         public static Day[] ConvertExcelToTimetable(string path)
         {
             XLWorkbook workbook = new XLWorkbook(path);
             var sheet = workbook.Worksheet(1);
 
+            // первая неделя или вторая
             string weekStart = sheet.Cell("a2").GetValue<string>();
-            // todo сделать определение, первая или вторая неделя
-            bool isFirst = false;
+            var weekStartWords = weekStart.Split();
 
-            //Console.WriteLine(DateTime.Now);
+            DateTime timetableStart = new DateTime();
+            int isTimetableStartEven = 0;
+
+            for (int i = 0; i < weekStartWords.Length; i++)
+            {
+                if (weekStartWords[i] == "марта" || weekStartWords[i] == "апреля" || weekStartWords[i] == "октября" || weekStartWords[i] == "ноября")
+                {
+                    timetableStart = new DateTime(DateTime.Now.Year, toMonth(weekStartWords[i]), Int32.Parse(weekStartWords[i - 1]));
+                }
+
+                if (weekStartWords[i] == "неделя")
+                    isTimetableStartEven = Int32.Parse(weekStartWords[i - 1]);
+            }
+
+            int dif = daysInMonths(DateTime.Now.Month) + DateTime.Now.Day - (daysInMonths(timetableStart.Month) + timetableStart.Day) + isTimetableStartEven * 7;
+
+            bool isFirst = (dif / 7) % 2 == 1;
+            
 
             Day[] Week = new Day[6];
             for (int i = 0; i < Week.Length; i++)

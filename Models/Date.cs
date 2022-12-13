c21662523eb8fd029b69a1e9af1d668fbef7499d
shabalin-AA <92672124+shabalin-AA@users.noreply.github.com>
2022-12-13 17:28:28 +0500

    Add files via upload

---
+++ Models/Date.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ReadExcel
+{
+    public class Date
+    {
+        public int day;
+        public int month;
+        public int year;
+
+        public Date() { }
+    }
+}
+
---
+++ Models/Lesson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ReadExcel
+{
+    public class Lesson
+    {
+        public string name = "";
+        public string time = "";
+
+
+        public Lesson() { }
+
+
+        public Lesson(string name, string time)
+        {
+            this.name = name;
+            this.time = time;
+        }
+    }
+}
+
